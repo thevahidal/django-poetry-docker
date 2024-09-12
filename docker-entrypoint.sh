7d65a66ed44a5927051c623a787cfceec3d8f5cd
Vahid Al <thevahidal@gmail.com>
2024-09-12 13:58:14 +0330

    Update docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,30 @@ echo "Waiting for database..."
 while ! nc -z ${SQL_HOST} ${SQL_PORT}; do sleep 1; done
 echo "Connected to database."
 
+python manage.py migrate --noinput
+if [ $? -ne 0 ]; then
+	echo "Migration failed." >&2
+	exit 1
+fi
+
+python manage.py import_templates
+if [ $? -ne 0 ]; then
+	echo "Importing templates failed." >&2
+	exit 1
+fi
+
 # if arguments passed, execute them
 # bring up an instance of project otherwise
-mkdir -p /var/log/projectname
+if [[ $# -gt 0 ]]; then
+	INPUT=$@
+	sh -c "$INPUT"
+else
+	mkdir -p /var/log/boogh
 
-python manage.py compilemessages
-python manage.py collectstatic --noinput
+	if [ "$DEBUG" = "True" ]; then
+		python manage.py collectstatic --noinput
+	fi
 
-echo "Starting Gunicorn..."
-exec gunicorn projectname.wsgi:application -c gunicorn.conf.py
+	echo "Starting Gunicorn..."
+	exec gunicorn boogh.wsgi:application -c gunicorn.conf.py
+fi
